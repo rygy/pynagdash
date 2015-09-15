ed768882e2c36788451a55f63f9f5124c6cc4622
RY <ryoung@spiderhop.net>
2015-09-15 09:17:48 -0500

    Initial

---
+++ pynagdash.py
@@ -0,0 +1,11 @@
+from pyxley import import UILayout
+from pyxley.filters import import SelectButton
+from pyxley.charts.mg import import LineChart, Figure
+from pyxley.utils import import FilterFrame
+
+ui = UILayout(
+        "FilterChart",
+        "./static/bower_components/pyxley/build/pyxley.js",
+        "component_id")
+
+
